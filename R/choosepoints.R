99296f7753a6df722f0b14fd00b0fea1d8539b62
melaniekamm <melanie.a.kammerer@gmail.com>
2018-10-10 15:19:08 -0400

    document subsetplots function

--- R/choosepoints.R
+++ R/choosepoints.R
@@ -1,21 +1,27 @@
-
+#'Subset sites based on proximity to ensure independence of bee communities
+#'
+#'@param points Spatial points object of sites (sampling locations)
+#'@param cutoff Distance cutoff for independent samples (typically average foraging range of bee(s))
+#'@export
+#'@examples
+#' Usage example coming soon.
 
 
 subsetplots <- function(points, cutoff) {
   repeat {
-    d <- distm(points)
+    d <- geosphere::distm(points)
     diag(d) <- -1
     d
-    #list of whittaker plots that are less than cutoff apart
+    #list of points that are less than cutoff apart
     closepoints <- data.frame(which(d >= 0 & d < cutoff, arr.ind=T))
     if (length(rownames(closepoints)) == 0) break
     else {temp <- data.frame(table(closepoints[2]))
-    #whittaker plots that have the maximum number of neighbors that are closer than cutoff (usually two neighbors)
+    #points that have the maximum number of neighbors that are closer than cutoff (usually two neighbors)
     removept <- sample(as.numeric(as.character(temp$Var1[temp$Freq == max(temp$Freq)])), 1)
-    
-    #whittaker plots subset which first one of very close plots removed
+
+    #subset points removing one of the worst points (with max neighbors closer than cutoff)
     points <- points[-removept,]
     }
   }
-  return(points)  
+  return(points)
 }
